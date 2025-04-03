f396051e83eef928875129c9355972e2972a3bcc
youngnamSHIN <youngnam151109@gmail.com>
2025-04-04 00:22:19 +0900

    Mybatisetest, MySQLConnectionTest

---
+++ src/main/java/my/first/mybatis/MySQLConnectionTest.java
@@ -0,0 +1,30 @@
+package my.first.mybatis;
+
+import java.sql.Connection;
+import javax.inject.Inject;
+import javax.sql.DataSource;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"file:src/main/webapp/WEB-INF/spring/**/root-context.xml"})
+
+public class MySQLConnectionTest {
+	
+	@Inject
+	private DataSource ds;
+	@Test
+	public void testConnection() throws Exception{
+		
+		try(Connection conn = ds.getConnection()){
+			System.out.println("\n >>>>>>>>>> Connection 출력 : " +conn+ "\n");
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
+	
+
